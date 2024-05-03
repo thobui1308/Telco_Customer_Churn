67907781f63ef6952e7ce8a869f47f552e20fd06
thobui1308 <thobui1308@gmail.com>
2024-05-03 15:52:08 +0700

    delete tab

--- pages/2_📑_Dataset.py
+++ pages/2_📑_Dataset.py
@@ -7,12 +7,7 @@ from streamlit_pandas_profiling import st_profile_report
 st.set_page_config(layout="wide")
 st.title(":bar_chart: Dataset")
 
-#tab1, tab2 = st.tabs(["Data Description", "Dashboard Data"])
-#with tab1:
 df = pd.read_excel("data.xlsx", index_col=0)
 df_profile = ProfileReport(df ,explorative=True)
 st_profile_report(df_profile)
-    #st.dataframe(df.head(20))
-#with tab2:
-    #df = pd.read_excel("df_dashboard.xlsx",index_col=0)
-    #st.dataframe(df.head(20))
+
