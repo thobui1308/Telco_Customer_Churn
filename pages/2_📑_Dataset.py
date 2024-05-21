be5079a21a4a33744173aa4f411485e33d7d89af
thobui1308 <thobui1308@gmail.com>
2024-05-21 16:05:56 +0700

    edit code

--- pages/2_📑_Dataset.py
+++ pages/2_📑_Dataset.py
@@ -9,13 +9,13 @@ df = pd.read_excel(root_path / "data" / "Data_Telco_Customer_Churn.xlsx", index_
 
 tab1, tab2, tab3 = st.tabs(["Demo", "Service","Status"])
 with tab1:
-    Demo = pd.read_excel(df, sheet_name = 'Telco_Demo')
+    Demo = pd.read_excel(root_path / "data" / "Data_Telco_Customer_Churn.xlsx", index_col=0, sheet_name = 'Telco_Demo')
     st.dataframe(Demo.head(20))
 with tab2:
-    Services = pd.read_excel(df, sheet_name = 'Telco_Services')
+    Services = pd.read_excel(root_path / "data" / "Data_Telco_Customer_Churn.xlsx", index_col=0, sheet_name = 'Telco_Services')
     st.dataframe(Services.head(20))
 with tab3:
-    Status = pd.read_excel(df, sheet_name = 'Telco_Status')
+    Status = pd.read_excel(root_path / "data" / "Data_Telco_Customer_Churn.xlsx", index_col=0, sheet_name = 'Telco_Status')
     st.dataframe(Status.head(20))
 
 
