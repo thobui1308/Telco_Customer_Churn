6ef59748db241999cd6be78020925a58753861fa
thobui1308 <thobui1308@gmail.com>
2024-05-21 15:58:52 +0700

    edit code

--- pages/2_📑_Dataset.py
+++ pages/2_📑_Dataset.py
@@ -1,14 +1,21 @@
 import pandas as pd
 import streamlit as st
-import numpy as np
 from pathlib import Path
-from ydata_profiling import ProfileReport
-from streamlit_pandas_profiling import st_profile_report
 
 st.set_page_config(layout="wide")
 st.title(":bar_chart: Dataset")
 root_path = Path(__file__).parent.parent # pages < root
-df = pd.read_excel(root_path / "data" / "data.xlsx", index_col=0)
-df_profile = ProfileReport(df ,explorative=True)
-st_profile_report(df_profile)
+df = pd.read_excel(root_path / "data" / "Data_Telco_Customer_Churn.xlsx", index_col=0)
+
+tab1, tab2, tab3 = st.tabs(["Demo", "Service","Status"])
+with tab1:
+    Demo = pd.read_excel(df, sheet_name = 'Telco_Demo')
+    st.dataframe(Demo.head(20))
+with tab2:
+    Services = pd.read_excel(df, sheet_name = 'Telco_Services')
+    st.dataframe(Services.head(20))
+with tab3:
+    Status = pd.read_excel(df, sheet_name = 'Telco_Status')
+    st.dataframe(Status.head(20))
+
 
