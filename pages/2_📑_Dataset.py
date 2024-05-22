93d9283b79ef6cf309563330e3911c2442750a5d
thobui1308 <thobui1308@gmail.com>
2024-05-22 08:55:46 +0700

    edit dataset page

--- pages/2_📑_Dataset.py
+++ pages/2_📑_Dataset.py
@@ -3,7 +3,7 @@ import streamlit as st
 import numpy as np
 from pathlib import Path
 from ydata_profiling import ProfileReport
-from streamlit_pandas_profiling import st_profile_report
+#from streamlit_pandas_profiling import st_profile_report
 
 st.set_page_config(layout="wide")
 st.title(":bar_chart: Dataset")
@@ -12,5 +12,5 @@ df = pd.read_excel("data.xlsx", index_col=0)
 root_path = Path(__file__).parent.parent # pages < root
 df = pd.read_excel(root_path / "data" / "data.xlsx", index_col=0)
 df_profile = ProfileReport(df ,explorative=True)
-st_profile_report(df_profile)
+#st_profile_report(df_profile)
 
